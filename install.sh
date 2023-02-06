3707a9fbd19dfb0049c542a7d4ae5ef42fdf56a4
darccau <darccau@gmail.com>
2023-02-05 23:51:37 -0300

    chore(install): add new tool from package manager

--- install.sh
+++ install.sh
@@ -1,45 +1,30 @@
-#!/usr/local/env bash                    
+#!/usr/bin/env bash
 
-BIN="$HOME/tools/taz/bin/"
+declare -A golang_tools
 
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-YELLOW='\033[0;33m '
-BLUE='\033[0;34m'
-
-
-[[ -d TOOLS_DIR ]] || echo "The directory $TOOLS_DIR does not exist"
-
-function pkg_manager { 
+function pkg_manager_tools {
   sudo apt install update -y
-  sudo DEBIAN_FRONTEND="noninteractive" apt install -y
-  golang \
-  python3 \
-  python3-pip \
-  curl \
-  sqlmap \
+  sudo apt install -y \
+    golang \
+    git \
+    python3 \
+    python3-pip \
+    curl
 }
 
+golang_tools['nuclei']='go install github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest'
+golang_tools['notify']='go install github.com/projectdiscovery/notify/cmd/notify@latest'
+golang_tools['httpx']='go install github.com/projectdiscovery/httpx/cmd/httpx@latest'
+golang_tools['dnsx']='go install github.com/projectdiscovery/dnsx/cmd/dnsx@latest'
+golang_tools['interactsh']='go install github.com/projectdiscovery/interactsh/cmd/interactsh-client@latest'
+golang_tools['tlsx']='go install github.com/projectdiscovery/tlsx/cmd/tlsx@latest'
+golang_tools['subfinder']='go install github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest'
+golang_tools['katana']='go install github.com/projectdiscovery/katana/cmd/katana@latest'
+golang_tools['naabu']='go install github.com/projectdiscovery/naabu/v2/cmd/naabu@latest'
+golang_tools['ffuf']='go install github.com/ffuf/ffuf@latest'
+golang_tools['anew']='go install github.com/tomnomnom/anew@lates'
+golang_tools['unfurl']='go install  github.com/tomnomnom/unfurl@latest'
 
-  tools['nuclei']='go install -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest' 
-  tools['notify']='go install -v github.com/projectdiscovery/notify/cmd/notify@latest'
-  tools['httpx']='go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest'
-  tools['dnsx']='go install -v github.com/projectdiscovery/dnsx/cmd/dnsx@latest'
-  tools['interactsh']='go install -v github.com/projectdiscovery/interactsh/cmd/interactsh-client@latest'
-  tools['tlsx']='go install github.com/projectdiscovery/tlsx/cmd/tlsx@latest'
-  tools['subfinder']='go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest'
-  tools['katana']='go install github.com/projectdiscovery/katana/cmd/katana@latest' tools['naabu']='go install -v github.com/projectdiscovery/naabu/v2/cmd/naabu@latest' tools['ffuf']='go install -v github.com/ffuf/ffuf@latest'
-  # tools['anew']='go install -v github.com/tomnomnom/anew@lates'
-  # tools['unfurl']='go install -v github.com/tomnomnom/unfurl@latest'
-  # tools['gau']='go install -v github.com/lc/gau/v2/cmd/gau@latest'
-  # tools['dalfox']='go install -v github.com/hahwul/dalfox/v2@latest'
-  # tools['Web-Cache-Vulnerability-Scanner']='go install -v github.com/Hackmanit/Web-Cache-Vulnerability-Scanner@latest'
-  # tools['jq']='go install -v github.com/tomnomnom/gf@latest'
-
-  for tool in "${tools[@]}"; do
-     $tool
-  done
-
-# validator 
-# golang_tools
-# echo -e "$YELLOW $GOPATH"
+for tool in "${golang_tools[@]}"; do
+	$tool
+done
